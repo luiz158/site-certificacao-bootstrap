0453773915c678be3aa914e85843a7d5e09649c1
Keven jesus <keven@Keven-MacBook-Pro.local>
2016-08-15 18:31:29 -0300

    remove funcitions check device

--- dist/js/app.js
+++ dist/js/app.js
@@ -5,19 +5,36 @@ function hideOffcanvas($element){
 	$element.removeClass('open-offcanvas');
 }
 
-$('.open-off').on('click', function(){
+function mobileEvents(){
+	$('.open-off').on('click', function(){
 	openOffcanvas($('html'));
-});
+	});
 
-$('html, body').on('swiperight', function(){
-	openOffcanvas($('html'));
-});
+	$('html, body').on('swiperight', function(){
+		openOffcanvas($('html'));
+	});
 
-$('html, body').on('swipeleft', function(){
-	hideOffcanvas($('html'));
-});
-
-$('html, body').on('click', function(e){
-	if(this === e.target)
+	$('html, body').on('swipeleft', function(){
 		hideOffcanvas($('html'));
+	});
+
+	$('html, body').on('click', function(e){
+		if(this === e.target)
+			hideOffcanvas($('html'));
+	});
+}
+
+function checkDeviceEvents($window){
+	if($window.width() <= 1024){
+		mobileEvents();
+	}
+}
+
+
+$(function(){
+	checkDeviceEvents($(window));
+	
+	$(window).resize(function(){
+		checkDeviceEvents($(this));	
+	});
 });
--- source/js/app.js
+++ source/js/app.js
@@ -5,19 +5,21 @@ function hideOffcanvas($element){
 	$element.removeClass('open-offcanvas');
 }
 
-$('.open-off').on('click', function(){
+function mobileEvents(){
+	$('.open-off').on('click', function(){
 	openOffcanvas($('html'));
-});
+	});
 
-$('html, body').on('swiperight', function(){
-	openOffcanvas($('html'));
-});
+	$('html, body').on('swiperight', function(){
+		openOffcanvas($('html'));
+	});
 
-$('html, body').on('swipeleft', function(){
-	hideOffcanvas($('html'));
-});
-
-$('html, body').on('click', function(e){
-	if(this === e.target)
+	$('html, body').on('swipeleft', function(){
 		hideOffcanvas($('html'));
-});
+	});
+
+	$('html, body').on('click', function(e){
+		if(this === e.target)
+			hideOffcanvas($('html'));
+	});
+}
